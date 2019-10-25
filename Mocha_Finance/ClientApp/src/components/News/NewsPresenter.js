a4cd680bf279793663218c4c08f4f8d32ec97587
Sungmin Kim <seongmin.kim082@gmail.com>
2019-10-25 15:07:00 -0400

    Put news block on News Component

--- Mocha_Finance/ClientApp/src/components/News/NewsPresenter.js
+++ Mocha_Finance/ClientApp/src/components/News/NewsPresenter.js
@@ -1,40 +1,12 @@
 import React from "react";
 import style from "./news.module.scss";
+import NewsBlock from "./NewsBlock";
 
 const NewsPresenter = () => {
   return (
     <section className={style.newsComponent}>
       <div className={style.container}>
-        <div className={style.row}>
-          <h2>7 Ways to Invest in the Energy Storage Boom</h2>
-          <div>Image</div>
-          <p>
-            Over the next seven years, Bank of America estimates energy storage
-            costs will decrease by 50%, ushering in the next phase of the global
-            transition to renewable energy. The firm estimates the energy
-            storage market could grow 16% annually...
-          </p>
-        </div>
-        <div className={style.row}>
-          <h2>7 Ways to Invest in the Energy Storage Boom</h2>
-          <div>Image</div>
-          <p>
-            Over the next seven years, Bank of America estimates energy storage
-            costs will decrease by 50%, ushering in the next phase of the global
-            transition to renewable energy. The firm estimates the energy
-            storage market could grow 16% annually...
-          </p>
-        </div>
-        <div className={style.row}>
-          <h2>7 Ways to Invest in the Energy Storage Boom</h2>
-          <div>Image</div>
-          <p>
-            Over the next seven years, Bank of America estimates energy storage
-            costs will decrease by 50%, ushering in the next phase of the global
-            transition to renewable energy. The firm estimates the energy
-            storage market could grow 16% annually...
-          </p>
-        </div>
+        <NewsBlock />
       </div>
     </section>
   );
