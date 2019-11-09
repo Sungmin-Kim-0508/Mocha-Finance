e9f020fed213ff1a0ef97a06b52593c89696f7fd
songjin900 <songjin900@gmail.com>
2019-11-09 14:28:23 -0500

    repush

--- Mocha_Finance/Model/StockDBContext.cs
+++ Mocha_Finance/Model/StockDBContext.cs
@@ -34,7 +34,6 @@ namespace Mocha_Finance.Model
                 .WithMany(b => b.MyFavourites)
                 .HasForeignKey(a => a.MemberID);
 
-
             modelBuilder.Entity<Stock>()
                 .HasOne(f => f.MyFavourite)
                 .WithMany(d => d.Stocks)
